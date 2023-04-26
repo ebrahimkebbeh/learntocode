003f531e7248f79198313d8a51b42b7f15f05b17
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-04-26 11:40:40 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

---
+++ workbook2/module2/tips.js
@@ -0,0 +1,13 @@
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00
+
+var totalDue = foodCost + tax + tip
+
+// console.log("the total due is " + totalDue)
+
+console.log(
+    'food cost is ' + foodCost + 'and tax is ' + tax);
+//  console.log('tip is ' + totalDue)
+console,log('tip is ' + tip.toFixed(2))
+console.log('total due is ' + totalDue)
