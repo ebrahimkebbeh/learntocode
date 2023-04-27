2b3f2d4c3d4435c0d686300e0f40fc518d8f0b35
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-04-27 09:39:04 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

---
+++ workbook2/module2/rentals.js
@@ -0,0 +1,15 @@
+var x = 38
+var vanc = 250
+var seat = 15
+
+var totalseat = x / seat
+var totalcost = 3 * vanc
+ 
+
+
+console.log("amount of van needed " + totalseat)
+console.log("total cost to rent the vans " + totalcost)
+
+var eachpassgerscost = totalcost / x
+
+console.log("each passgers pay " + eachpassgerscost)
