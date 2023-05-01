26063bd1a6dbd572c616cc71121a8dd6f2f1eb24
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-01 11:35:37 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

---
+++ workbook3/functions/simple_fun.js
@@ -0,0 +1,12 @@
+function favoriteThings(){
+    console.log("Ebrahima Kebbeh, Godfather, Better")
+
+}
+
+function whyImHere(){
+    console("i joined this learning to code program because i have an intrest for computer and i want learn how creat thing things in a computer")
+}
+
+function favortPlace(){
+    console("my favorite place to vist is the Gambia because thats where im from, and also i have families there that I have seen for long period")
+}
