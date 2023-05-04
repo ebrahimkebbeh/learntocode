050c90f12287a0964065ce0c9db0b3348f9f9ca2
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-04 13:54:38 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

--- workbook3/acmecarrental/rate.js
+++ workbook3/acmecarrental/rate.js
@@ -7,16 +7,16 @@ function getRateData(days, tag, gps, roadside, under25){
     //logic
     rentalAmount = 29.99 * days;
     if(tag){
-        optionsAmount = 3.95 * days;
+        optionsAmount += 3.95 * days;
     }
     if(gps){
-        optionsAmount = 2.95 * days;
+        optionsAmount += 2.95 * days;
     }
     if(roadside){
-        optionsAmount = 2.95 * days;
+        optionsAmount += 2.95 * days;
     }
     if(under25){
-        surchargeAmount = rentalAmount * 0.30;
+        surchargeAmount += rentalAmount * 0.30;
     }
 
     totalDue = rentalAmount + optionsAmount +surchargeAmount
@@ -33,14 +33,14 @@ function getRateData(days, tag, gps, roadside, under25){
 //test
 const results = getRateData( 7, true, true, true, false)
 
-function displayResult(results){
-console.log(`
+function getTexrResult(results){
+    return`
     car Rental:         $${results.rentalAmount.toFixed(2)}
     options:            $${results.optionsAmount.toFixed(2)}
     under 25 surcharge: $${results.surchargeAmount.toFixed(2)}
 
     car Rental:         $${results.totalDue.toFixed(2)}
-`);
+`;
 }
-displayResult(getRateData(10, true, true, true, true))
-displayResult(getRateData(10, false, false, false, false))
+console.log(getTexrResult(getRateData(10, true, true, true, true)))
+console.log(getTexrResult(getRateData(10, false, false, false, false)))
