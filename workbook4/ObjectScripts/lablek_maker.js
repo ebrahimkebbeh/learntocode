ba02d2dbba83b295a085cde694811f7b3ec4e7ce
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-09 10:04:22 -0400

    https://github.com/ebrahimkebbeh/WB4-exercises.git

---
+++ workbook4/ObjectScripts/lablek_maker.js
@@ -0,0 +1,19 @@
+
+let info = {
+    name: "ebrahima",
+    adress: "1360 webster ave,",
+    city: "bronx",
+    zip: 10456
+}
+function printContact(contact){
+    console.log(`
+    ${contact.name}
+    ${contact.adress}
+    ${contact.city}
+    ${contact.zip}
+
+    `)
+}
+
+printContact(info)
+
