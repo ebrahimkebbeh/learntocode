59b5d866e0d501aee55480004ebf446891f1ff8e
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-18 11:41:34 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

--- workbook5/ts.js
+++ workbook5/ts.js
@@ -24,3 +24,20 @@
 //     else return 1;
 // });
 // console.log(numbers)
+
+
+// 
+
+let products = [
+    {prodId: 2, item: "Notepads (12 pk)", price: 12.29},
+    {prodId: 12, item: "Black Pens (12 pk)", price: 5.70},
+    {prodId: 22, item: "Stapler", price: 12.79}
+    ];
+
+    products.sort(function(a,b){
+        if (a.item > b.item) retrun -1;
+        else if (a.item == b.item) return  0;
+        else return 1,
+
+        
+    })
