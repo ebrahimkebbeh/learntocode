4f9d07c57da9d3ddfd753d45997ff8449d7db526
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-19 13:36:27 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

--- workbook5/exercisse/script/main.js
+++ workbook5/exercisse/script/main.js
@@ -3,6 +3,8 @@
 function showimage(path){
     const dialogImage = document.querySelector(".dialog .gigantic")
     dialogImage.src = path;
+
+    document.querySelector(".dialog").classList.remove('hidden')
 }    
 
 document.addEventListener('DOMContentLoaded', ()=>{
