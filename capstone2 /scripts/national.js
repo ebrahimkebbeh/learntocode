c3eb9bc0a86420fc01fb43cacd27023f326b6ad5
ebrahimkebbeh <130486625+ebrahimkebbeh@users.noreply.github.com>
2023-05-25 10:47:32 -0400

    https://github.com/ebrahimkebbeh/learntocode.git

---
+++ capstone2 /scripts/national.js
@@ -0,0 +1,101 @@
+let select_park = document.getElementById('select_park')
+        
+for(i = 0; i < parkTypesArray.length; i++){
+    let o = parkTypesArray[i];
+    const option = document.createElement('option')
+    option.innerHTML = o;
+    option.value = o;
+    select_park.appendChild(option)
+}
+
+let select_location = document.getElementById('select_location')
+
+for(i = 0; i < locationsArray.length; i++){
+    let o = locationsArray[i];
+    const option = document.createElement('option')
+    option.innerHTML = o;
+    option.value = o;
+    select_location.appendChild(option)
+}
+
+//location names start
+
+function display(park){
+    results.innerHTML += `
+    
+    <tr><td>${park.LocationName}</td></tr>
+    
+    `
+}
+function list(){
+    let text = select_park.selectedOptions[0].value
+    let location = select_location.selectedOptions[0].value
+    let parks = nationalParksArray
+    if(location != 'any'){
+        parks = parks.filter(p => p.State==location)
+    }
+    if(text != 'any'){
+        parks = parks.filter(p => p.LocationName.includes(text))
+    }
+    results.innerHTML = ""
+    parks.forEach(display)
+}
+// list()
+select_park.addEventListener('change', list)
+select_location.addEventListener('change', list)
+
+//end of locaation name
+
+// function display(locat){
+//     results.innerHTML += `
+
+//     <tr><td>${locat.Address}</td></tr>
+
+//     `
+// }
+
+// function ress(){
+//     let text = select_park.selectedOptions[1].value
+//     let address = select_location.selectedOptions[1].value
+//     let add = nationalParksArray
+//     if(address != 'any'){
+//         add = add.filter(p => p.City==address)
+//     }
+//     if(text != 'any'){
+//         add = add.filter(p => p.Address.includes(text))
+//     }
+//     results.innerHTML = ""
+//     add.forEach(display)
+// }
+// select_park.addEventListener('change', ress)
+// select_location.addEventListener('change', ress)
+
+
+
+
+
+
+// parkTypeArray.forEach(text => {
+//     console.log(text)
+//     let o = document.creayeElemnt('option')
+//     o.innerHTML = text
+//     o.value = text
+//     select_park.appendChild(o)
+// })
+
+// for(let i = 0; i < parkTypesArray; i++){
+//     let park = mountainsArray[i];
+//     let o = document.createElement('option')
+//     o.text = park.parkTypesArray
+//     o.value = park.parkTypesArray
+//     select_park.appendChild(o)
+// }
+
+//         let park = parkTypesArray
+
+// park.forEach(function(item){
+//     let o=document.createElement('option');
+//     o.text = item.parkTypesArray
+//     o.value = item.parkTypesArray
+//     select_park.appendChild(o)
+// });
--- capstone2 /scripts/parkTypeData.js
+++ capstone2 /scripts/parkTypeData.js
@@ -13,3 +13,4 @@ const parkTypesArray = [
     "Trail",
     "Parkway"
 ]
+
